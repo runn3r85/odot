a61a3eff631659d832412f5c888c81bb39deb585
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-15 17:08:25 -0800

    add helper to display new_item link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def new_item_link
+    if @todo_list && !@todo_list.new_record?
+      text = "Todo Item"
+      path = new_todo_list_todo_item_path(@todo_list)
+    else
+      text = "Todo List"
+      path = new_todo_list_path
+    end
+    link_to "Add #{text}", path, class: "icon-new right hide-text"
+  end
 end
